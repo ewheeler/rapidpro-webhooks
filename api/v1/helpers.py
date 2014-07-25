9d51a3a06ef758895e057a3ec832300fc447f97a
ewheeler <evanmwheeler@gmail.com>
2014-07-25 14:05:11 +0300

    add slugify helper
    add unidecode to requirements

--- api/v1/helpers.py
+++ api/v1/helpers.py
@@ -2,6 +2,7 @@ import operator
 import re
 import socket
 
+from unidecode import unidecode
 from flask import url_for
 from flask import request
 
@@ -144,3 +145,15 @@ def is_valid_ipv6_address(address):
     except socket.error:  # not a valid address
         return False
     return True
+
+
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+
+def slugify(text, delim=u'-'):
+    """Generates an ASCII-only slug.
+       Graciously cribbed from http://flask.pocoo.org/snippets/5/ """
+    result = []
+    for word in _punct_re.split(text.lower()):
+        result.extend(unidecode(word).split())
+    return unicode(delim.join(result))
