5bb8d6939937058fd8fb861f7812b3ea7c5e9cc2
Kenneth <matovken@gmial.com>
2016-02-11 17:12:43 +0300

    get rid of server name

--- settings/prod.py
+++ settings/prod.py
@@ -1,5 +1,5 @@
 from settings.base import *
-SERVER_NAME='webhooks.uniceflabs.org'
+# SERVER_NAME='webhooks.uniceflabs.org'
 SERVER_PORT = 5050
 DEBUG = False
 LOG_FILE = '/var/log/webhooks/errors.log'
