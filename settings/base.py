180553183f8b7208f3cccf89a938d8723ed3e2ee
Kenneth <matovken@gmial.com>
2016-07-11 15:34:04 +0300

    adding fusion table export

--- settings/base.py
+++ settings/base.py
@@ -3,3 +3,6 @@ JSON_AS_ASCII=False
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost/webhooks"
 DEBUG = True
 PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+GOOGLE_SERVICE_EMAIL = 'ft-451@rapidpro-ft.iam.gserviceaccount.com'
+RAPIDPRO_EMAIL = 'rapidprodata@gmail.com'
