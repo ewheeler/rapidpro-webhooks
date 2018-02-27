58ce5dbd53e4fb8a87ad67c9ba4c8720a8fcc30b
ewheeler <evanmwheeler@gmail.com>
2018-02-27 12:58:26 -0500

    add sentry config

--- settings/prod.py
+++ settings/prod.py
@@ -3,3 +3,4 @@ from settings.base import *
 SERVER_PORT = 5050
 DEBUG = False
 LOG_FILE = '/var/log/webhooks/errors.log'
+SENTRY_DSN = 'https://15d326cf18eb4d328a486a2e91a8759f:d6479a6dc40143b79aaf54fdfedda4e9@sentry.io/281376'
